19447e1e7abffce20fe8fe8f27efb863a31af39a
Asif Mehmood <asif-mehmood@hotmail.com>
2023-05-23 17:58:34 +0500

    complete dynamic pdf from db

--- controller/pdfController.js
+++ controller/pdfController.js
@@ -2,6 +2,20 @@ const { getdataFromDb } = require('../model/dataModel');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const path = require('path');
+
+const hbs = require('handlebars');
+
+const fs_extra = require('fs-extra');
+
+// const data = require('../data.json');
+
+// const compile = async function (templateName, data) {
+//   const filePath = path.join(process.cwd(), 'templates', `${templateName}.hbs`);
+
+//   const html = await fs_extra.readFile(filePath, 'utf8');
+//   return hbs.compile(html)(data);
+// };
+
 exports.loadPdf = async (req, res) => {
   try {
     res.sendFile('pdf.html', { root: './views/' });
@@ -12,14 +26,39 @@ exports.loadPdf = async (req, res) => {
 
 exports.generatePdf = async (req, res) => {
   try {
+    const rows = await getdataFromDb();
+    const data = {
+      title: 'User Data',
+      users: rows,
+    };
+    console.log(data);
+
+    const compile = async function (templateName, data) {
+      const filePath = path.join(
+        process.cwd(),
+        'templates',
+        `${templateName}.hbs`
+      );
+
+      const html = await fs_extra.readFile(filePath, 'utf8');
+      return hbs.compile(html)(data);
+    };
+
     const browser = await puppeteer.launch({
       headless: false,
     });
 
     const page = await browser.newPage();
-    await page.goto('http://localhost:4000/api/loadPdf', {
-      waitUntil: 'networkidle2',
-    });
+
+    const content = await compile('index', data);
+
+    await page.setContent(content);
+
+    console.log('done creating pdf');
+
+    // await page.goto('http://localhost:4000/api/loadPdf', {
+    //   waitUntil: 'networkidle2',
+    // });
 
     // `${req.protocol}://${req.get('host')`+ "/loadPdf`}`
 
@@ -33,53 +72,20 @@ exports.generatePdf = async (req, res) => {
       format: 'A4',
     });
 
-    // await browser.close();
-
     const pdfURL = path.join(
       __dirname,
       '../files',
       todayDate.getTime() + '.pdf'
     );
 
-    // res.set({
-    //   'Content-Type': 'application/pdf',
-    //   'Content-Length': pdfn.length,
-    // });
-
-    // res.sendFile(pdfURL);
-
     res.download(pdfURL, err => {
       if (err) {
         console.log(err);
       }
     });
+
+    // await browser.close();
   } catch (error) {
     console.log(error);
   }
 };
-// exports.generatePdf = async (req, res) => {
-//   try {
-// let html = fs.readFileSync('./views/pdf.html', 'utf-8');
-
-// let data = {
-//   '{{NAME}}': 'asif',
-//   '{{CONTACT}}': '4545',
-//   '{{EMAIL}}': 'goog@gmail.com',
-// };
-// html = html.replace(/{{NAME}} | {{CONTACT}} | {{EMAIL}}/gi, matched => {
-//   return data[matched];
-// });
-
-//     let options = {
-//       format: 'Letter',
-//     };
-
-//     pdf.create(html, options).toFile('./data.pdf', (err, resp) => {
-//       if (err) return console.log(err);
-
-//       console.log(resp);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
